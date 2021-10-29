46f449e0209ee6cd3ffc87ce278c8a3dc6bd0f1f
funcode <funcococode@gmail.com>
2021-10-29 17:07:13 +0530

    added minutes options

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import './App.css';
 import Timer from './Components/Timer';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import bg1 from "./imgs/bg-1.svg";
+import bg2 from "./imgs/bg-2.png";
+import bg3 from "./imgs/bg-3.png";
+import bg4 from "./imgs/bg-4.png";
+
+
+
 
 function App() {
-  let [minutes,setMinutes] = useState(5);
-  return (
-    <div className="App">
-      <Timer minutes={minutes} />
-    </div>
-  );
+  	let [minutes,setMinutes] = useState(5);
+	let [bgStyle,setBgStyle] = useState({
+		backgroundImage :`url(${bg1})`,
+	});
+	return (
+		<div className="App">
+		<aside id="sideBar">
+			<ul>
+				<li className="sidebar-item"><ion-icon name="ellipse-outline"></ion-icon> Focus</li>
+				<li className="sidebar-item"><ion-icon name="egg-outline"></ion-icon> Short Break</li>
+				<li className="sidebar-item"><ion-icon name="balloon-outline"></ion-icon> Long Break</li>
+				<li className="sidebar-item">
+					<div className="bg-group">
+						<figure className="bg-option currentBG">
+							<img src={bg1} data-bg="1" onClick={()=>setBgStyle({backgroundImage:`url(${bg1})`})}/>
+						</figure>
+						<figure className="bg-option">
+							<img src={bg2} data-bg="2" onClick={()=>setBgStyle({backgroundImage:`url(${bg2})`})}/>
+						</figure>
+						<figure className="bg-option">
+							<img src={bg3} data-bg="3" onClick={()=>setBgStyle({backgroundImage:`url(${bg3})`})}/>
+						</figure>
+						<figure className="bg-option">
+							<img src={bg4} data-bg="4" onClick={()=>setBgStyle({backgroundImage:`url(${bg4})`})}/>
+						</figure>
+					</div>
+				</li>
+			</ul>
+
+		</aside>
+		<div style={bgStyle}>
+			<div id="rightTop">
+				<a onClick={()=>setMinutes(5)}>5M</a>
+				<a onClick={() => setMinutes(10)}>10M</a>
+				<a onClick={()=>setMinutes(20)}>20M</a>
+				<a onClick={()=>setMinutes(30)}>30M</a>
+				<a onClick={()=>setMinutes(45)}>45M</a>
+			</div>
+			<div id="rightBottom">
+				<Timer minutes={minutes} />
+			</div>
+		</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -3,17 +3,23 @@ import React, { Component } from 'react'
 export default class Timer extends Component {
 	constructor(props){
 		super(props);
-		this.storedSeconds = props.minutes * 60;
-		this.totalSeconds = props.minutes * 60;
 		this.updateTimer = this.updateTimer.bind(this);
-		
 		this.state = {
 			isActive : false,
+			storedSeconds : props.minutes * 60,
+			totalSeconds : props.minutes * 60,
 			seconds : "00",
-			minutes :this.totalSeconds / 60,
+			minutes :"00",
 
 			currentState : <ion-icon name="play-outline"></ion-icon>
 		}
+	}
+
+	componentDidMount(){
+
+		this.setState({
+			minutes : ~~(this.state.totalSeconds / 60)
+		})
 	}
 
 	handleClick = () =>{
@@ -32,11 +38,12 @@ export default class Timer extends Component {
 			this.setState({
 				isActive:false,
 				currentState:<ion-icon name="play-outline"></ion-icon>,
-				minutes : this.storedSeconds / 60,
+				minutes : this.state.storedSeconds / 60,
 				seconds : "00"
 
 			},()=>{
 				clearInterval(this.timerId);
+				document.title = "Pomodo!"
 			})
 		}
 	}
@@ -45,18 +52,28 @@ export default class Timer extends Component {
 		clearInterval(this.timerId);
 	}
 	
-	componentDidUpdate(){
+	componentDidUpdate(prevProps){
+		if(prevProps.minutes !== this.props.minutes){
+			console.log(prevProps,this.props.minutes)
+			this.setState({
+				totalSeconds: this.props.minutes * 60,
+				minutes : ~~(this.props.minutes)
+			});
+		}
+
 		if(this.state.minutes === 0 && this.state.seconds === 0 && this.state.isActive){
+
 			this.resetTimer();
 		}
+			document.title = `${this.state.minutes}:${this.state.seconds} - Focus!`
 	}
 
 	updateTimer(){
 		if(this.state.isActive){
-			this.totalSeconds--;
+			this.state.totalSeconds--;
 			this.setState({
-				seconds : ~~(this.totalSeconds % 60),
-				minutes : ~~(this.totalSeconds / 60),	
+				seconds : ~~(this.state.totalSeconds % 60),
+				minutes : ~~(this.state.totalSeconds / 60),	
 				currentState:<ion-icon name="pause-outline"></ion-icon>
 			})
 
@@ -73,6 +90,9 @@ export default class Timer extends Component {
 				<div id='bottomContainer'>
 					<button onClick={this.handleClick} id='btn'>{this.state.currentState}</button>
 					<button onClick={this.resetTimer} id='resetBtn'><ion-icon name="refresh-outline"></ion-icon></button>
+				</div>
+				<div id="lockUnlockIcon">
+					{this.state.isActive ? <ion-icon name="lock-closed-outline"></ion-icon> : <ion-icon name="lock-open-outline"></ion-icon>}
 				</div>
 			</div>
 		)
